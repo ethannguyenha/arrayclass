1b4e6f792c9401a750b048f1dc4cdaf7df594b6f
ethannguyenha <43502463+ethannguyenha@users.noreply.github.com>
2018-10-06 12:30:37 -0700

    day2
    day2

---
+++ Arrayclass.java
@@ -0,0 +1,57 @@
+import java.util.Arrays;
+
+public class Arrayclass 
+{
+  
+  public static void main(String [] args)
+    
+  {
+    /*
+    int [] num = new int [10];
+    
+    int num1 = 1;
+    int num2 = 2;
+    int num3 = 3;
+    int num4 = 4;
+    int num5 = 5;
+    int num6 = 6;
+    int num7 = 7;
+    int num8 = 8;
+    int num9 = 9;
+    int num10 = 10;
+    
+    
+    num[0] = num1;
+    num[1] = num2;
+    num[2] = num3;
+    num[3] = num4;
+    num[4] = num5;
+    num[5] = num6;
+    num[6] = num7;
+    num[7] = num8;
+    num[8] = num9;
+    num[9] = num10;
+    
+    for (int loopVal = 0; loopVal <10; loopVal++) {
+      System.out.println("Array Value: " + int[loopVal]);
+  
+        String [] words = new String [5];
+        words[0] = "Hi";
+        words[1] = "my";
+        words[2] = "name";
+        words[3] = "is";
+        words[4] = "Bob the Builder!";
+       
+    int arraySize = 5;
+    for (int loopVal = 0; loopVal <arraySize; loopVal++) {
+      System.out.println("Array Value: " + words[loopVal]);
+      **/
+    
+    int a = 5;
+    int b = 7;
+    if (a > b)
+      System.out.println(a);
+    if (a < b)
+      System.out.println(b);
+    }
+  }
